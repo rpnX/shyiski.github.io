964e604b80dc425935661c3dc2e2724034501376
rpnX <55102634+rpnX@users.noreply.github.com>
2020-03-13 14:23:39 +0300

    Updates

---
+++ precache-manifest.e921abe1c2de38340c4f9a85820fa24d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24a7066647e13892e3282712682fbb5",
+    "url": "/reactfilm/index.html"
+  },
+  {
+    "revision": "ca793f3ae1e6a1b51235",
+    "url": "/reactfilm/static/css/main.ac40f505.chunk.css"
+  },
+  {
+    "revision": "a9a3655e8afc28d2cd54",
+    "url": "/reactfilm/static/js/2.7f421567.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactfilm/static/js/2.7f421567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca793f3ae1e6a1b51235",
+    "url": "/reactfilm/static/js/main.70ead296.chunk.js"
+  },
+  {
+    "revision": "22aa52a0970a7a3e0d1d",
+    "url": "/reactfilm/static/js/runtime-main.5c6e9abd.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/reactfilm/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/reactfilm/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
